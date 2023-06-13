60d0d06f1e8540a21359abf47bc3ecf192272597
IanTaiAhn <iantaiahn@yahoo.com>
2023-06-13 11:27:16 -0600

    Try again.

--- backend/interior_design/interior_design/settings.py
+++ backend/interior_design/interior_design/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default=sec_key)
+# SECRET_KEY = os.environ.get('SECRET_KEY', default=sec_key)
+SECRET_KEY = sec_key
 
-# SECRET_KEY = sec_key
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-
+DEBUG = False
+# turn on to true when published
 ALLOWED_HOSTS = []
 
 
