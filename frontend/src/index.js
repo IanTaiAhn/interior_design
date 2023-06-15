0918feedb4b5b9d62445eefa4d5113e2ee6188ae
IanTaiAhn <iantaiahn@yahoo.com>
2023-06-15 11:40:01 -0600

    Updated urls and deprecated api.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import FrontendApi from "./api/FrontendApi";
-import RandomImage from "./api/RandomImgApi";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
