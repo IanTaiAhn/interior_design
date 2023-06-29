ea2abec7766efc16088b2a8fa839afa3f1089ac5
IanTaiAhn <iantaiahn@yahoo.com>
2023-06-28 20:04:29 -0600

    app is now the production folder

---
+++ app/interior_design/urls.py
@@ -0,0 +1,32 @@
+"""interior_design URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from main.views import DataViewSet, upload_image
+from django.conf.urls.static import static
+from django.conf import settings
+
+router = routers.SimpleRouter()
+# I call this api on react by typing data into by axios fetch.
+router.register('data', DataViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    # path('api/randimg/', get_random_image, name='randimg'),
+    path('upload/', upload_image, name='upload_image'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ app/main/views.py
@@ -0,0 +1,71 @@
+import os
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from rest_framework import viewsets
+from PIL import Image, ImageFilter
+from .serializers import DataSerializer
+from .models import TestData
+from django.views.decorators.csrf import ensure_csrf_cookie
+# For testing
+
+
+class DataViewSet(viewsets.ModelViewSet):
+    queryset = TestData.objects.all()
+    serializer_class = DataSerializer
+
+# Here for code help, other than that deprecated.
+# def get_random_image(request):
+#     api_key = random_img_api_key
+#     url = 'https://api.unsplash.com/photos/random'
+#     headers = {
+#         'Authorization': f'Client-ID {api_key}'
+#     }
+#     params = {
+#         'query': 'nature'  # Modify the query to suit your needs
+#     }
+#     response = requests.get(url, headers=headers, params=params)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return JsonResponse(data)
+#     else:
+#         return JsonResponse({'error': 'Failed to fetch image'}, status=500)
+
+
+def process_image(image_path, iterations):
+    image = Image.open(image_path)
+    for i in range(iterations):
+        image = image.filter(ImageFilter.BLUR)
+    image.save(image_path)
+    return image
+
+
+# @ensure_csrf_cookie
+# @csrf_protect
+@csrf_exempt
+def upload_image(request):
+    if request.method == 'POST' and request.FILES.get('image'):
+        uploaded_image = request.FILES['image']
+        save_location = 'media/images/' + uploaded_image.name
+
+        with open(save_location, 'wb') as f:
+            for chunk in uploaded_image.chunks():
+                f.write(chunk)
+
+        processed_image = process_image(save_location, 4)
+
+        img_extension = os.path.splitext(save_location)
+        print(img_extension[1])
+        if img_extension[1] == ".png":
+            response = HttpResponse(content_type="image/png")
+            processed_image.save(response, "PNG")
+        elif img_extension[1] == ".jpeg" or img_extension[1] == ".jpg":
+            response = HttpResponse(content_type="image/jpeg")
+            processed_image.save(response, "JPEG")
+
+        os.remove(save_location)
+
+        return response
+    else:
+        return HttpResponse("Error: Upload failed", status=400)
