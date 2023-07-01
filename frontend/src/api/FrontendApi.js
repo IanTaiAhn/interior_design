ed4acaa91338354c6e824fa307f83704917e1144
IanTaiAhn <iantaiahn@yahoo.com>
2023-07-01 16:40:09 -0600

    I'll just check with the live server

--- frontend/src/api/FrontendApi.js
+++ frontend/src/api/FrontendApi.js
@@ -8,8 +8,8 @@ const FrontendApi = () => {
 
   useEffect(() => {
     axios
-      // .get("https://web-production-a9bb.up.railway.app/data")
-      .get("http://127.0.0.1:8000/check")
+      .get("https://web-production-a9bb.up.railway.app/check")
+      // .get("http://127.0.0.1:8000/check")
       .then((response) => {
         setData(response.data);
       })
