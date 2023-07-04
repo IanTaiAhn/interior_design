4b14edc3c8ad5e562eed9807faf04687726d570b
IanTaiAhn <iantaiahn@yahoo.com>
2023-07-04 13:01:19 -0600

    Untracking backend files.

--- backend/interior_design/main/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import TestData
-
-
-class DataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TestData
-        fields = ['id', 'name', 'description']
