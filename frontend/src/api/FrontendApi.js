7970e51b1ccb8b929bdff76b29ddbecbf4147bb1
IanTaiAhn <iantaiahn@yahoo.com>
2023-07-15 17:45:23 -0600

    Ready to test the user creation from the frontend side.

--- frontend/src/api/FrontendApi.js
+++ frontend/src/api/FrontendApi.js
@@ -1,10 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ClientImg from "../img_input/ClientImg";
+// This is base for all of the react components that show on the webpage.
 
 const FrontendApi = () => {
   const [data, setData] = useState([]);
+  const handleClick = () => {
+    // Your method logic here
+    console.log("Button clicked!");
+    createUser(userData);
+  };
   useState(false);
+
+  // create User method
+  const createUser = async (userData) => {
+    try {
+      const response = await axios.post(
+        "https://web-production-a9bb.up.railway.app/create_user",
+        userData
+      );
+      console.log(response.data); // User created successfully
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Usage
+  const userData = {
+    username: "exampleUser",
+    password: "examplePassword",
+    email: "example@ex.com",
+    tokens: 5,
+  };
 
   useEffect(() => {
     axios
@@ -12,6 +39,7 @@ const FrontendApi = () => {
       .get("https://web-production-a9bb.up.railway.app/health")
       .then((response) => {
         setData(response.data);
+        // we create a user if the health check works
       })
       .catch((error) => {
         console.error(error);
@@ -20,6 +48,7 @@ const FrontendApi = () => {
   return (
     <div>
       <h1>Health Check</h1>
+      <button onClick={handleClick}>Click Me</button>
       {/* Display the fetched data */}
       {data && <p>{JSON.stringify(data)}</p>}
       <ClientImg />
